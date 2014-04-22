50657bb04e266b72c089701a5f9bab6fc9839ae2
Thomas Tursics <thomas@tursics.de>
2014-04-22 21:53:20 +0200

    added own geocoded

--- www/scripts/dragndrop.js
+++ www/scripts/dragndrop.js
@@ -3,9 +3,6 @@
 */
 
 // -----------------------------------------------------------------------------
-// http://www.html5rocks.com/en/tutorials/file/dndfiles/
-// http://www.htmlgoodies.com/html5/javascript/drag-files-into-the-browser-from-the-desktop-HTML5.html#fbid=NRudD56hfes
-// -----------------------------------------------------------------------------
 
 function initDragnDrop()
 {
@@ -48,6 +45,7 @@ function dndHandleDragOver( evt)
 		'<div style="margin:2em 4em 2em 4em;text-shadow:none;">' +
 		'<div style="font-size:3em;text-align:center;margin-bottom:0.5em;"><i class="fa fa-file-text-o"></i></div>' +
 		'Du hast eine Datei? Klasse, dann lasse sie auf der Karte fallen.' +
+		'<br><center><a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-a" data-rel="back">Abbrechen</a></center>' +
 		'</div>');
 	$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
 
@@ -95,16 +93,20 @@ function dndHandleNewFile( f)
 	} else if( f.type.match( 'application/pdf')) {
 		errorImg = 'fa-file';
 		errorTxt = 'Eine PDF-Datei, so, so. Was soll ich damit?';
+	} else if( f.type.match( 'application/json')) {
+		// accepted
 	} else if( !f.type.match( 'text.*')) {
 		errorImg = 'fa-file-o';
 		errorTxt = 'Ist diese Datei Open Data freundlich?';
+//		errorTxt += ' (' + f.type + ')';
 	}
 
 	if( 0 != errorTxt.length) {
 		$( '#popupDrop').html(
 			'<div style="margin:2em 4em 2em 4em;text-shadow:none;">' +
 			'<div style="font-size:3em;text-align:center;margin-bottom:0.5em;"><i class="fa ' + errorImg + '"></i></div>' +
-			errorTxt + ' Bitte nur CSV-Dateien hier her ziehen.' +
+//			errorTxt + ' Bitte nur CSV-Dateien hier her ziehen.' +
+			errorTxt + ' Bitte nur JSON-Dateien hier her ziehen.' +
 			'</div>');
 		$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
 
@@ -148,39 +150,20 @@ function dndReadUpdateProgress( evt)
 
 // -----------------------------------------------------------------------------
 
-var dndReader = null;
-
 function dndReadTextFile( f)
 {
-//	progress.style.width = '0%';
-//	progress.textContent = '0%';
-
-	dndReader = new FileReader();
-	dndReader.onerror = dndReadErrorHandler;
-	dndReader.onprogress = dndReadUpdateProgress;
-	dndReader.onabort = function( e) {
-		alert( 'File read cancelled');
-	};
-	dndReader.onloadstart = function( e) {
-//		document.getElementById( 'progress_bar').className = 'loading';
-	};
-	dndReader.onload = function( e) {
-		// Ensure that the progress bar displays 100% at the end.
-//		progress.style.width = '100%';
-//		progress.textContent = '100%';
-//		setTimeout( "document.getElementById( 'progress_bar').className='';", 2000);
-		$( '#popupDrop').html(
-			'<div style="margin:0;text-shadow:none;">' +
-			e.target.result +
-			'</div>');
-		$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
+	var reader = new FileReader();
+	reader.onloadend = function( e) {
+		if( e.target.readyState == FileReader.DONE) {
+			if( '{' == e.target.result.substring( 0, 1)) {
+				dndReadFileJSON( e.target.result);
+			}
+		}
 	}
 
 	var str = '';
 	str += '<strong>' + escape( f.name) + '</strong><br>';
-	str += (f.type || 'n/a') + '<br>';
-	str += f.size + ' bytes<br>';
-	str += 'Last modified: ' + (f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a');
+	str += 'Lade jetzt ' + f.size + ' Bytes...<br>';
 
 	$( '#popupDrop').html(
 		'<div style="margin:2em 4em 2em 4em;text-shadow:none;">' +
@@ -189,7 +172,138 @@ function dndReadTextFile( f)
 		'</div>');
 	$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
 
-	dndReader.readAsText( f);
+//	var blob = f.slice( 0, 1000);
+//	reader.readAsText( blob);
+	reader.readAsText( f);
+}
+
+// -----------------------------------------------------------------------------
+
+var dndReadFileObject = null;
+var dndReadFileIndex = 0;
+var dndReadFileGeocodeSet = null;
+
+function dndReadFileJSON( stream)
+{
+	var obj = $.parseJSON( stream);
+
+	if( typeof obj != 'undefined') {
+		if( typeof obj.index != 'undefined') {
+			dndReadFileObject = obj;
+			dndReadFileIndex = 0;
+
+			if( dndReadFileGeocodeSet) {
+				map.objects.remove( dndReadFileGeocodeSet);
+			}
+			dndReadFileGeocodeSet = new nokia.maps.map.Container();
+			map.objects.add( dndReadFileGeocodeSet);
+
+			$( '#popupDrop').html(
+				'<div style="margin:2em 4em 2em 4em;text-shadow:none;">' +
+				'<div style="font-size:3em;text-align:center;margin-bottom:0.5em;"><i class="fa fa-table"></i></div>' +
+				'Verteile die Punkte auf der Karte' +
+				'</div>');
+			$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
+
+			dndReadFileObjectFunc();
+		} else {
+			$( '#popupDrop').html(
+				'<div style="margin:2em 4em 2em 4em;text-shadow:none;">' +
+				'<div style="font-size:3em;text-align:center;margin-bottom:0.5em;"><i class="fa fa-table"></i></div>' +
+				'Diese Datei ist leider nicht lesbar.' +
+				'</div>');
+			$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
+		}
+	} else {
+		$( '#popupDrop').html(
+			'<div style="margin:2em 4em 2em 4em;text-shadow:none;">' +
+			'<div style="font-size:3em;text-align:center;margin-bottom:0.5em;"><i class="fa fa-table"></i></div>' +
+			'Diese Datei ist leider nicht lesbar.' +
+			'</div>');
+		$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
+	}
+}
+
+// -----------------------------------------------------------------------------
+
+function dndReadFileObjectFunc()
+{
+	try {
+		if( typeof dndReadFileObject.index[dndReadFileIndex] == 'undefined') {
+			$( '#popupDrop').popup( 'close');
+		} else {
+			var street = '';
+			var zip = '';
+			var city = '';
+
+			if( typeof dndReadFileObject.index[dndReadFileIndex].anschrift != 'undefined') {
+				street = dndReadFileObject.index[dndReadFileIndex].anschrift;
+			}
+			if( typeof dndReadFileObject.index[dndReadFileIndex].strasse != 'undefined') {
+				street = dndReadFileObject.index[dndReadFileIndex].strasse;
+			}
+			if( typeof dndReadFileObject.index[dndReadFileIndex].plz != 'undefined') {
+				zip = dndReadFileObject.index[dndReadFileIndex].plz;
+			}
+			if( typeof dndReadFileObject.index[dndReadFileIndex].ort != 'undefined') {
+				city = dndReadFileObject.index[dndReadFileIndex].ort;
+			}
+			if( typeof dndReadFileObject.index[dndReadFileIndex].plz_ort != 'undefined') {
+				zip = dndReadFileObject.index[dndReadFileIndex].plz_ort.substring( 0, 5);
+				city = dndReadFileObject.index[dndReadFileIndex].plz_ort.substring( 6);
+			}
+
+			var jqxhr = $.ajax( 'scripts/geocoding.php?street=' + encodeURIComponent( street) + '&zip=' + encodeURIComponent( zip) + '&city=' + encodeURIComponent( city))
+			.done( function( data) {
+				var result = $.parseJSON( data);
+
+				if( typeof result.errorCode != 'undefined') {
+					switch( parseInt( result.errorCode)) {
+					case 10:
+						// Could not geocode given address
+						$( '#popupDrop').html(
+							'<div style="margin:2em 4em 2em 4em;text-shadow:none;">' +
+							'<div style="font-size:3em;text-align:center;margin-bottom:0.5em;"><i class="fa fa-table"></i></div>' +
+							'Diese Adresse konnte nicht geocodiert werden:<br><br>' + street + '<br>' + zip + ' ' + city +
+							'</div>');
+						$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
+						return;
+					case 12:
+						// Zip is outside Berlin-Lichtenberg
+						delete result.error;
+						break;
+					}
+				}
+				if( typeof result.error != 'undefined') {
+					$( '#popupDrop').html(
+						'<div style="margin:0;text-shadow:none;">' +
+						data + '<br>' + street + ', ' + zip + ' ' + city +
+//						this.url +
+						'</div>');
+					$( '#popupDrop').popup( 'reposition', {positionTo: "window"});
+					return;
+				}
+
+				if( typeof result.lat != 'undefined') {
+					marker = new nokia.maps.map.StandardMarker([parseFloat( result.lat), parseFloat( result.lon)], { text: dndReadFileIndex + 1 });
+					dndReadFileGeocodeSet.objects.add( marker);
+					map.zoomTo( dndReadFileGeocodeSet.getBoundingBox(), false);
+				}
+
+				++dndReadFileIndex;
+				dndReadFileObjectFunc();
+			})
+//			.fail( function() {
+//				alert( "error" );
+//			})
+//			.always( function() {
+//				alert( "complete" );
+//			})
+			;
+		}
+	} catch( e) {
+		alert( e);
+	}
 }
 
 // -----------------------------------------------------------------------------
---
+++ www/scripts/geocoding.php
@@ -0,0 +1,154 @@
+<?php
+	// -------------------------------------------------------------------------
+	// Datenformatbeschreibung Hauskoordinaten Deutschland
+	// Version: 3.1
+	// Date:    2013-07-01
+	// Format:  ISO 8859/1 (ISO Latin-1)
+	// -------------------------------------------------------------------------
+	// Sample:
+	// N;DEBE000000740554;A;11;0;00;011;1101;40148;7;;13,520049;52,508027;Am Tierpark;10315;Berlin;;;;16.04.2014;
+	// -------------------------------------------------------------------------
+	// NBA (Kennung des Datensatzes)                       [1 char]
+	//   "N" neu, "L" zu löschen, "A" geändert
+	//   N
+	// OI (Eindeutige Nummer des Datensatzes)              [string, 16 chars]
+	//   DEBE000000740554
+	// QUA (Qualität der georeferenzierten Gebäudeadresse) [1 char]
+	//   Koordinaten "A" innerhalb Gebäude, "B" innerhalb Flurstück, "R" Gebäude evtl. nicht vorhanden
+	//   A
+	// LAN (Schlüssel Land)                                [2 chars]
+	//   11
+	// RBZ (Schlüssel Regierungsbezirk)                    [1 char]
+	//   0
+	// KRS (Schlüssel Kreis/kreisfreie Stadt)              [2 chars]
+	//   00
+	// GMD (Schlüssel Gemeinde)                            [3 chars]
+	//   011
+	// OTT (Schlüssel des Orts- bzw. Gemeindeteils)        [4 chars]
+	//   1101
+	// SSS (Schlüssel der Straße)                          [5 chars]
+	//   40148
+	// HNR (Hausnummer)                                    [string]
+	//   7
+	// ADZ (Adressierungszusatz)                           [string]
+	//
+	// (1. Koordinatenwert ETRS89/UTM, East-Wert)          [fixed 8,3]
+	//   13,520049
+	// (2. Koordinatenwert ETRS89/UTM, North-Wert)         [fixed 7,3]
+	//   52,508027
+	// STN (unverschlüsselter Straßenname)                 [string]
+	//   Am Tierpark
+	// PLZ (Postleitzahl)                                  [string, 5 chars]
+	//   10315
+	// ONM (Postalischer Ortsname)                         [string]
+	//   Berlin
+	// ZON (Zusatz zum postalischen Ortsnamen)             [string]
+	//
+	// POT (Postalischer Ortsteil)                         [string]
+	//
+	// ???
+	//
+	// ???
+	//   16.04.2014
+	// -------------------------------------------------------------------------
+
+	$ret = array();
+
+	if( !isset( $_GET[ 'street'])) {
+		$ret[ error] = "Parameter 'street' missing.";
+	} else {
+		$street = mb_strtolower( utf8_decode( $_GET[ 'street']));
+		$streetNumber = '';
+		$streetAdd = '';
+		if( preg_match( '/(.+)\s([\d]+)([^\d]*)/i', $street, $result)) {
+			$street = trim( $result[1]);
+			$streetNumber = trim( $result[2]);
+			$streetAdd = trim( $result[3]);
+		}
+		// „Süße Früchtchen“ Wiecker Straße 9
+		if( utf8_decode( '„') == substr( $street, 0, 1)) {
+			$street = substr( $street, strpos( $street, utf8_decode( '“'), 1) + 1);
+		}
+		// "Erieseering 4 - 6" => "Erieseering 4"
+		// "Erieseering 4 / 6" => "Erieseering 4"
+		if(( '-' == substr( $street, -1)) || ('/' == substr( $street, -1))) {
+			if( preg_match( '/(.+)\s([\d]+)/i', $street, $result)) {
+				$street = trim( $result[1]);
+				$streetNumber = trim( $result[2]);
+			}
+		}
+		// "Erieseering 4-6" => "Erieseering 4"
+		// "Erieseering 4/6" => "Erieseering 4"
+		if(( '-' == $streetAdd) || ('/' == $streetAdd)) {
+			$streetAdd = '';
+		}
+		// Privatstraße 7, Nr. 18
+		if( ', nr.' == substr( $street, -5)) {
+			$street = substr( $street, 0, -5);
+		}
+		// Privatstraße 7 Nr. 18
+		if( 'nr.' == substr( $street, -3)) {
+			$street = substr( $street, 0, -3);
+		}
+		// "Charlottenstr." => "Charlottenstraße"
+		if( 'str.' == substr( $street, -4)) {
+			$street = substr( $street, 0, -1) . utf8_decode( 'aße');
+		}
+		// "Josef \u2013 Orlopp \u2013 Str." => "Josef - Orlopp - Str."
+		$street = str_replace ( utf8_decode( '–'), '-', $street);
+		$street = preg_replace( '/\s+/', '', $street);
+
+		if( !isset( $_GET[ 'zip'])) {
+			$ret[ error] = "Parameter 'zip' missing.";
+		} else {
+			$zip = utf8_decode( $_GET[ 'zip']);
+
+			if( !in_array(( int) $zip, array( 10315, 10317, 10318, 10319, 10365, 10367, 10369, 13051, 13053, 13055, 13057, 13059))) {
+				$ret[ error] = "Zip is outside Berlin-Lichtenberg";
+				$ret[ errorCode] = 12;
+			} else {
+				if( !isset( $_GET[ 'city'])) {
+					$ret[ error] = "Parameter 'city' missing.";
+				} else {
+					$city = utf8_decode( $_GET[ 'city']);
+
+					$path = "../data/HKO_Lichtenberg_Geographisch_140416.txt";
+					if( !file_exists( $path)) {
+						$ret[ error] = "Internal error.";
+					} else {
+						$content = explode( "\n", file_get_contents( $path));
+
+						foreach( $content as $line) {
+							$rows = explode( ";", $line);
+							if( count( $rows) > 15) {
+								$dataHNR = mb_strtolower( $rows[9]);
+								$dataADZ = mb_strtolower( $rows[10]);
+								$dataLon = str_replace( ',', '.', $rows[11]);
+								$dataLat = str_replace( ',', '.', $rows[12]);
+								$dataSTN = preg_replace( '/\s+/', '', mb_strtolower( $rows[13]));
+								$dataPLZ = $rows[14];
+								$dataONM = $rows[15];
+
+								if(( $dataSTN == $street) && ($dataHNR == $streetNumber) && ($dataADZ == $streetAdd)) {
+									$ret[ street] = utf8_encode( $rows[13] . ' ' . $rows[9] . $rows[10]);
+									$ret[ zip] = utf8_encode( $dataPLZ);
+									$ret[ city] = utf8_encode( $dataONM);
+									$ret[ lon] = $dataLon;
+									$ret[ lat] = $dataLat;
+								}
+							}
+						}
+
+						if( count( $ret) == 0) {
+							$ret[ error] = "Could not geocode given address.";
+							$ret[ errorCode] = 10;
+							$ret[ error] = "Could not geocode given address: " . utf8_encode( $street) . " | $streetNumber | $streetAdd";
+						}
+					}
+				}
+			}
+		}
+	}
+
+	echo json_encode( $ret);
+?>
