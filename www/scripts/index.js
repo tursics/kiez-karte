edd4fc5f006252cf28cae53ed8c7b060dcdefb1d
Thomas Tursics <thomas@tursics.de>
2015-02-14 16:16:09 +0100

    change map provider
    change from Nokia Here Maps to leaflet and mapbox

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -8,79 +8,17 @@ var dataAge = -1;
 var dataGeoSet = new Array();
 
 // -----------------------------------------------------------------------------
-/*
-Spielplatzbestand Berlin
-Karte, 1:1.000 
-nutzIII.pdf
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=K.gris_spielplatz%40senstadt
 
-Spielplatzversorgung - öffentlich
-Karte, 1:5.000 
-Nutzungsbedingungen noch nicht abschließend formuliert
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=K.spielver_oeff%40senstadt
-
-Spielplatzversorgung - öffentlich und privat
-Karte, 1:5.000 
-Nutzungsbedingungen noch nicht abschließend formuliert
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=K.spielver_oeffpriv%40senstadt
-
-Spielplatzbestand Berlin
-Sachdaten, 1845 Daten
-nutzIII.pdf
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=S.s_spielplatz%40senstadt
-http://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_spielplatz?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=1.1.0
-
-Spielplatzversorgung - öffentlich / PLR
-Sachdaten, 438 Daten
-Nutzungsbedingungen noch nicht abschließend formuliert
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=S.s_spielver_oe_plr%40senstadt
-
-Spielplatzversorgung - öffentlich / VE
-Sachdaten, 1354 Daten
-Nutzungsbedingungen noch nicht abschließend formuliert
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=S.s_spielver_oe_ve%40senstadt
-
-Spielplatzversorgung - öffentlich und privat / PLR
-Sachdaten, 438 Daten
-Nutzungsbedingungen noch nicht abschließend formuliert
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=S.s_spielver_oepr_plr%40senstadt
-
-Spielplatzversorgung - öffentlich und privat / VE
-Sachdaten, 1354 Daten
-Nutzungsbedingungen noch nicht abschließend formuliert
-http://fbinter.stadt-berlin.de/fb/gisbroker.do;jsessionid=8327EAA7A1442FD8EA33B1D195F90868?cmd=navigationShowResult&mid=S.s_spielver_oepr_ve%40senstadt
-
-*/
-// -----------------------------------------------------------------------------
-
-function initNokiaMap( elementName, lat, lng, zoom)
+function initMap( elementName, lat, lng, zoom)
 {
-	// https://developer.here.com/myapps
-	// PLAN: Free
-	// url: http://tursics.de/sample/lichtenberg
-	// limit: 100,000 monthly transactions
-	nokia.Settings.set( 'app_id', 'EsMBz0OPmROKzmgArmcd');
-	nokia.Settings.set( 'app_code', '3Df8lHy0T7q98SCPI_-Vmw');
-
-	// test key for
-	// url: http://tursics.de/sample/lichtenberg
-	// until: 2014-06-27
-	nokia.Settings.set( 'app_id', 'R69qfu8j1eZyNk50CekA');
-	nokia.Settings.set( 'app_code', '6GwB7JeP2QyKOEUy911pZw');
-
-	map = new nokia.maps.map.Display(
-		document.getElementById( elementName), {
-			components: [
-				new nokia.maps.map.component.Behavior(),
-				new nokia.maps.map.component.ZoomBar(),
-//				new nokia.maps.map.component.TypeSelector(),
-				// ScaleBar Overview ZoomRectangle Positioning ContextMenu InfoBubbles PublicTransport Traffic
-			],
-			zoomLevel: zoom,
-			center: [lat, lng],
-			baseMapType: nokia.maps.map.Display.NORMAL // NORMAL NORMAL_COMMUNITY SATELLITE SATELLITE_COMMUNITY  SMARTMAP SMART_PT TERRAIN TRAFFIC
+	var mapboxToken = 'pk.eyJ1IjoidHVyc2ljcyIsImEiOiI1UWlEY3RNIn0.U9sg8F_23xWXLn4QdfZeqg';
+	var mapboxTiles = L.tileLayer( 'https://{s}.tiles.mapbox.com/v4/tursics.l7ad5ee8/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
+		attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a> | <a href="/imprint">Impressum</a> | <a href="/copyright">Copyright</a> | <a href="/imprint">Kontakt</a>'
 	});
-//	map.removeComponent( map.getComponentById( "zoom.MouseWheel"));
+
+	map = L.map( elementName)
+		.addLayer( mapboxTiles)
+		.setView( [lat, lng], zoom);
 }
 
 // -----------------------------------------------------------------------------
@@ -88,7 +26,7 @@ function initNokiaMap( elementName, lat, lng, zoom)
 $( document).on( "pagecreate", "#pageMap", function()
 {
 	// center the city hall
-	initNokiaMap( 'mapContainer', 52.515807, 13.479470, 16);
+//	initMap( 'mapContainer', 52.515807, 13.479470, 16);
 
 	$( '#displayBaby').on( 'click', function( e) {
 		setAge( 0);
@@ -107,48 +45,19 @@ $( document).on( "pagecreate", "#pageMap", function()
 	});
 
 	$( '#displayNormal').on( 'click', function( e) {
-		map.setBaseMapType( map.NORMAL, 'default');
+//		map.setBaseMapType( map.NORMAL, 'default');
 	});
 	$( '#displaySatelite').on( 'click', function( e) {
-		map.setBaseMapType( map.SATELLITE, 'default');
+//		map.setBaseMapType( map.SATELLITE, 'default');
 	});
 	$( '#displayCar').on( 'click', function( e) {
-		map.setBaseMapType( map.TRAFFIC, 'default');
+//		map.setBaseMapType( map.TRAFFIC, 'default');
 	});
 	$( '#displayBus').on( 'click', function( e) {
-		map.setBaseMapType( map.SMART_PT, 'default');
+//		map.setBaseMapType( map.SMART_PT, 'default');
 	});
 
 	initDragnDrop();
-
-//	var marker1 = new nokia.maps.map.StandardMarker([52.515807, 13.479470], {
-//		text: "Hi"
-//	});
-//	map.objects.add( marker1);
-
-//	var marker2 = new nokia.maps.map.Marker(
-//		new nokia.maps.geo.Coordinate(52.515, 13.481), {
-//			title: "marker",
-//			visibility: true,
-//			icon: "images/pin_restaurant.png",
-//			anchor: new nokia.maps.util.Point(16, 36)
-//		}
-//	);
-//	map.objects.add( marker2);
-
-/*	map.objects.add( new nokia.maps.map.Circle(
-		[52.51, 13.49],
-		500,
-		{
-			pen: {
-				strokeColor: "#C22A",
-				lineWidth: 2
-			},
-			brush: {
-				color: "#0FF6"
-			}
-		}
-	));*/
 
 	updateMapSelectData();
 
@@ -213,6 +122,9 @@ function createFTPLink( str)
 
 $( document).on( "pageshow", "#pageMap", function()
 {
+	// center the city hall
+	initMap( 'mapContainer', 52.515807, 13.479470, 16);
+
 	if( showWelcome) {
 		showWelcome = false;
 		$( '#welcomeClose').on( 'click', function( e) {
@@ -221,106 +133,6 @@ $( document).on( "pageshow", "#pageMap", function()
 		$( '#popupWelcome').popup( 'open');
 	}
 });
-
-// -----------------------------------------------------------------------------
-
-/*function sample1()
-{
-	setTimeout( function () {
-		function onTransitionFinished1() {
-			map.removeListener( "transitionend", onTransitionFinished1);
-
-			sample2();
-		}
-
-		map.addListener( "transitionend", onTransitionFinished1);
-		map.setZoomLevel( 15, 'default');
-	}, 1000);
-}*/
-
-// -----------------------------------------------------------------------------
-
-/*function sample2()
-{
-	setTimeout( function () {
-		function onTransitionFinished2() {
-			map.removeListener( "basemapchangeend", onTransitionFinished2);
-
-			sample3();
-		}
-
-		map.addListener( "basemapchangeend", onTransitionFinished2);
-		map.setBaseMapType( map.SMART_PT, 'default');
-	}, 1000);
-}*/
-
-// -----------------------------------------------------------------------------
-
-/*function sample3()
-{
-	setTimeout( function () {
-		function onTransitionFinished3() {
-			map.removeListener( "basemapchangeend", onTransitionFinished3);
-
-			sample4();
-		}
-
-		map.addListener( "basemapchangeend", onTransitionFinished3);
-		map.setBaseMapType( map.SATELLITE, 'default', {
-			latitude: 52.515,
-			longitude: 13.481,
-			zoom: 16
-		});
-	}, 1000);
-}*/
-
-// -----------------------------------------------------------------------------
-
-/*function sample4()
-{
-	// http://developer.here.com/javascript-apis/documentation/maps/topics/overlays.html
-	// http://heremaps.github.io/examples/explorer.html#find-user-address
-}*/
-
-// -----------------------------------------------------------------------------
-
-/*function geocode( term)
-{
-	nokia.places.search.manager.geoCode({
-		searchTerm: term,
-		onComplete: geocodeResults
-	});
-}*/
-
-// -----------------------------------------------------------------------------
-
-/*var geocodeResultSet;
-var geocodeResults = function( data, requestStatus, requestId)
-{
-	var i, len, locations, marker;
-
-	if( requestStatus == "OK") {
-		locations = data.results ? data.results.items : [data.location];
-		if( locations.length > 0) {
-			if( geocodeResultSet) {
-				map.objects.remove( geocodeResultSet);
-			}
-
-			geocodeResultSet = new nokia.maps.map.Container();
-			for( i = 0, len = locations.length; i < len; ++i) {
-				marker = new nokia.maps.map.StandardMarker( locations[i].position, { text: i+1 });
-				geocodeResultSet.objects.add( marker);
-			}
-
-			map.objects.add( geocodeResultSet);
-//			map.zoomTo( geocodeResultSet.getBoundingBox(), false);
-		} else { 
-			alert( "Your search produced no results!");
-		}
-	} else {
-		alert( "The search request failed");
-	}
-};*/
 
 // -----------------------------------------------------------------------------
 
@@ -806,8 +618,6 @@ function setAge( age)
 function onShowData( dataId, ageId)
 {
 	if( dataId < dataVec.length) {
-		var TOUCH = nokia.maps.dom.Page.browser.touch;
-		var CLICK = TOUCH ? 'tap' : 'click';
 		var colorOut = {color: '#155764'};
 		var colorOver = {color: '#8C9C88'};
 
@@ -827,23 +637,23 @@ function onShowData( dataId, ageId)
 		dataGeoSet.push({
 			id: dataId,
 			age: ageId,
-			container: new nokia.maps.map.Container()
+			layerGroup: L.featureGroup([])
 		});
 
-		var container = dataGeoSet[ dataGeoSet.length - 1].container;
+		var layerGroup = dataGeoSet[ dataGeoSet.length - 1].layerGroup;
 
-		container.addListener( CLICK, function( evt) {
-			updateMapSelectItem( evt.target.data);
-		}, false);
-		container.addListener( 'mouseover', function( evt) {
-			evt.target.set( 'brush', colorOver);
-			map.update( -1, 0);
-		}, false);
-		container.addListener( 'mouseout', function( evt) {
-			evt.target.set( 'brush', colorOut);
-			map.update( -1, 0);
-		}, false);
-		map.objects.add( container);
+		layerGroup.addEventListener( 'click', function( evt) {
+			updateMapSelectItem( evt.layer.options.data);
+		});
+//		layerGroup.addEventListener( 'mouseover', function( evt) {
+//			evt.target.set( 'brush', colorOver);
+//			map.update( -1, 0);
+//		});
+//		layerGroup.addEventListener( 'mouseout', function( evt) {
+//			evt.target.set( 'brush', colorOut);
+//			map.update( -1, 0);
+//		});
+		layerGroup.addTo(map);
 
 		updateMapSelectData();
 
@@ -855,16 +665,16 @@ function onShowData( dataId, ageId)
 			try {
 				$.each( data, function( key, val) {
 					if((typeof val.lat != 'undefined') && (typeof val.lng != 'undefined')) {
-						marker = new nokia.maps.map.StandardMarker([parseFloat( val.lat), parseFloat( val.lng)], {
-							brush: colorOut,
+						var marker = L.marker([parseFloat( val.lat), parseFloat( val.lng)],{
+//							brush: colorOut,
 							data: val
 						});
-						container.objects.add( marker);
+						layerGroup.addLayer( marker);
 					}
 				});
 
-				if( container.objects.getLength() > 0) {
-					map.zoomTo( container.getBoundingBox(), false);
+				if( layerGroup.getLayers().length > 0) {
+					map.fitBounds( layerGroup.getBounds());
 				}
 			} catch( e) {
 //				alert( e);
@@ -878,7 +688,7 @@ function onShowData( dataId, ageId)
 function onRemoveData( dataId)
 {
 	if( dataId < dataGeoSet.length) {
-		map.objects.remove( dataGeoSet[ dataId].container);
+		map.removeLayer( dataGeoSet[ dataId].layerGroup);
 		dataGeoSet.splice( dataId, 1);
 	}
 
@@ -886,7 +696,4 @@ function onRemoveData( dataId)
 	updateMapSelectData();
 }
 
-// -----------------------------------------------------------------------------
-//	map.addListener( "basemapchangestart", function () {});
-//	map.addListener( "basemapchangeend", function () {});
 // -----------------------------------------------------------------------------
